231635463b340eb4929f5d020c98f1a02c0d430a
汪寒成 <171094685@qq.com>
2015-07-07 16:59:27 +0800

    Do some modification in search and insert.

--- tree/BTtree.c
+++ tree/BTtree.c
@@ -4,35 +4,52 @@
 
 typedef struct BTreeNode{
 	int n;//n个关键字
-	int key[m];//key from 1 to m-1
-	struct BTreeNode *child[m];//child from 0 to m-1
+	int *key;//key from 1 to m-1
+	struct BTreeNode **child;//child from 0 to m-1
 	struct BTreeNode *parent;
 }BTNode;
 
-//插入
-void insertIntoBTtree(){
-	
-}
+
 
 //查找
 bool searchInBTtree(BTNode *pRoot,int key,BTNode **pRes,int *index){
-	if(NULL == pRoot)
+	if(NULL == pRoot){
 		return false;
+	}
 	int i = 0;
-	for(i=1; i<=n; i++){
+	for(i=1; i<=pRoot->n; i++){
+		*pRes = pRoot;
+		*index = i;
 		if(pRoot->key[i] == key){
-			*pRes = pRoot;
-			*index = i;
 			return true;
 		}
 		else if(pRoot->key[i] > key){
 			return searchInBTtree(pRoot->child[i-1],key,pRes,index);
 		}
-		else if(i == n){
+		else if(i == pRoot->n){
 			return searchInBTtree(pRoot->child[i],key,pRes,index);
 		}
 	}
 }
+
+//插入
+bool insertIntoBTtree(BTNode *pRoot,int key){
+	BTNode **pNode = NULL;
+	int index;
+	if(searchInBTtree(pRoot,key,pNode,index))
+		return false;
+	if(pNode->n < m-1){//该结点还有空位
+		int i;
+		for(i=n+1; i>index; i--){
+			pNode->key[i] = pNode->key[i-1];
+		}
+		pNode->key[i] = key;
+	}
+	else{//该结点没有空位，需要拆分
+		
+	}
+}
+
 
 //删除
 bool deleteFromBTtree(){
