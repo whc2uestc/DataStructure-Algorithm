c661fcff614acb3a3ffcdb3b0e4d4eefd920ff3e
whc <whc@whc-desktop.(none)>
2015-07-12 22:51:44 +0800

    the data structure of queue.

--- queue/linkedDeque.c
+++ queue/linkedDeque.c
@@ -1,26 +1,77 @@
+/* linked queue
+ * author:whc
+ * 2015.7.12*/
+
 #include <stdio.h>
 
+/* the struct of the linked queue.*/
 typedef struct ListNode{
     int element;
     int *next;
 }ListNode;
+typedef struct LinkedQueue{
+    int size;
+    ListNode *front;
+    ListNode *back;
+}LinkedQueue;
 
-void pop(){
 
+/* alloc a node.*/
+ListNode* allocNode(int element){
+    ListNode *pNode = (ListNode*)malloc(sizeof(ListNode));
+    if(pNode){
+        pNode->element = elemnt;
+        pNode->next = NULL;
+    }
+    return pNode;
 }
 
-void push(){
 
+/* alloc a queue.*/
+LinkedQueue* allocLinkedQueue(){
+    LinkedQueue *pQueue = (LinkedQueue*)malloc(sizeof(LinkedQueue));
+    if(pQueue){
+        pQueue->size = 0;
+        pQueue->front = NULL;
+        pQueue->back = NULL;
+    }
+    return pQueue;
 }
 
-int top(){
-
+/* pop a node out of the queue.*/
+bool pop(LinkedQueue *pQueue){
+    if(NULL == pQueue)
+       return false;
+    LinkedQueue *pNode = pQueue->front;
+    if(pQueue->front == pQueue->back)
+        pQueue->back = NULL;
+    pQueue->front = pQueue->front->next;
+    free(pNode);
+    pQueue->size --;
+    return true;
 }
 
-int size(){
-
+/* push a node into the queue.*/
+bool push(LinkedQueue *pQueue,int element){
+    if(NULL == pQueue)
+        return false;
+    pQueue->size ++;
+    if(NULL == pQueue->front){
+        pQueue->front = pQueue->back = allocNode(element);
+    }
+    else
+        pQueue->back = allocNode(element);
+    return true;
 }
 
-int main(){
 
+/* get the element from the queue.*/
+bool front(LinkedQueue *pQueue,int *element){
+    if(pQueue->size){
+        *element = pQueue->front->element;
+        return true;
+    }
+    else
+        return false;
 }
+
