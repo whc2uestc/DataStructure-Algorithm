639a51e84d01f31a50b4aa5148d02ad55b54631c
whc <whc@whc-desktop.(none)>
2015-07-12 22:17:45 +0800

    the data structure of queue.

---
+++ queue/linkedDeque.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef struct ListNode{
+    int element;
+    int *next;
+}ListNode;
+
+void pop(){
+
+}
+
+void push(){
+
+}
+
+int top(){
+
+}
+
+int size(){
+
+}
+
+int main(){
+
+}
