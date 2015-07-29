5323522f56f1db584899f2e9fb1c8d97ac334a01
汪寒成 <171094685@qq.com>
2015-07-29 18:38:05 +0800

    the algorithm of select sort.

--- sort/selectSort.c
+++ sort/selectSort.c
@@ -2,7 +2,6 @@
 
 void selectSort(int data[], int n){
 	int outer,inner;
-	
 	for(outer = n-1; outer > 0; outer -- ){
 		int max = data[outer];
 		int index = outer;
@@ -15,13 +14,11 @@ void selectSort(int data[], int n){
 		data[index] = data[outer];
 		data[outer] = max;
 	}
-
 }
 
 int main(){
 	int data[10] = {9,6,3,2,5,8,4,7,1,0};
 	int i;
-	
 	selectSort(data,10);
 	for(i = 0; i < 10; i ++)
 		printf("%d ",data[i]);
