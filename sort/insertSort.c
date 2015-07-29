6d3ab9fe2bb0d26a8a337971192931cb07d6f8ba
汪寒成 <171094685@qq.com>
2015-07-29 18:37:21 +0800

    the algorithm of insert sort.

--- sort/insertSort.c
+++ sort/insertSort.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 void insertSort(int data[],int n){
-	
 	int outer,inner;
-	
 	for(outer = 1; outer < n; outer ++){
 		int temp = data[outer];
 		for(inner = outer; inner > 0 && temp < data[inner-1]; inner --){
@@ -11,13 +9,11 @@ void insertSort(int data[],int n){
 		}
 		data[inner] = temp;
 	}
-
 }
 
 int main(){
 	int data[10] = {9,6,3,2,5,8,4,7,1,0};
 	int i;
-	
 	insertSort(data,10);
 	for(i = 0; i < 10; i ++)
 		printf("%d ",data[i]);
