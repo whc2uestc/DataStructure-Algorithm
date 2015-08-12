9884f829351a02e2a110e34227c55e9af4632fc2
汪寒成 <171094685@qq.com>
2015-08-12 22:20:51 +0800

    update the data structure of heap.

--- heap/heap.c
+++ heap/heap.c
@@ -1,17 +1,46 @@
 #include <vector>
-
-vector<int> heap;
-
+#include <stdio.h>
+using namespace std;
+/* Big root heap.
+ * 大根堆
+ */
+void swap(int *a,int *b){
+	*a = (*a)^(*b);
+	*b = (*a)^(*b);
+	*a = (*a)^(*b);
+}
 /* init the heap.*/
-void init(){
+void sift(vector<int> &heap,int index){
+	int temp = heap[index];
+	while((index+1)*2 <= heap.size()){
+		if((index+1)*2==heap.size() && temp<heap[(index+1)*2-1]){
+			heap[index] = heap[(index+1)*2-1]);
+			break;
+		}
+		else if(index*2<heap.size()){
+			int max = heap[(index+1)*2]>heap[(index+1)*2-1]?(index+1)*2:(index+1)*2-1;
+			if(x < heap[max]){
+				heap[index] = heap[max];
+				index = max;
+			}else
+				break;
+		}else{
+			break ;
+		}
+	}
+	heap[index] = temp; 
+}
+void init(vector<int> &heap,int index){
 	if(heap.empty())
 		return ;
-	int i,len = heap.size();
-	
+	int i;
+	for(i=heap.size()/2; i>=0; i--){
+		sift(heap,i);
+	}
 }
 
 /* insert the element into the heap.*/
-void insert(int element){
+void insert(vector<int> &heap,int element){
 	heap.push_back(element);
 	int len = heap.size();
 	int i = len-1;
@@ -26,28 +55,35 @@ void insert(int element){
 }
 
 /* get the top element.*/
-bool top(int *res){
+bool top(vector<int> &heap,int *res){
 	if(heap.empty())
 		return false;
 	*res = heap[0];
-	int len = heap.size()-1;
-	heap[0] = heap[len];
-	int i = 0;
-	while(2*(i+1) < len){
-		int max = heap[2*(i+1)-1]<heap[2*(i+1)]?2*(i+1):(2*(i+1)-1);
-		if(heap[i] > heap[max])
-			return true;
-		heap[i] = heap[i]^heap[max];
-		heap[max] = heap[i]^heap[max];
-		heap[i] = heap[i]^heap[max];
-		i = max;
-	}
-	if(2*(i+1)== len && heap[i]<heap[2*(i+1)-1]){
-		heap[i] = heap[i]^heap[2*(i+1)-1];
-		heap[2*(i+1)-1] = heap[i]^heap[2*(i+1)-1];
-		heap[i] = heap[i]^heap[2*(i+1)-1];
-	}
+	sift(heap,0);
 	return true;
+}
+
+
+void sort(vector<int> &vec){
+	init(vec);
+	int len = vec.size();
+	while(len--){
+		int num;
+		top(vec,&num);
+		printf("%d ",num);
+	}
+}
+
+int main(){
+	vector<int> vec;
+	int i = 10;
+	while(i--){
+		vec.push_back(i);
+	}
+	sort(vec);
+	for(i=0; i<10; i++)
+		printf("%d ",vec[i]);
+	return 0;
 }
 
 
