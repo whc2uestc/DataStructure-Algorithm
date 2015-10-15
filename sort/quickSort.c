a9d9784a38b8d8495286bf8d82c7575d6e61cae8
HC.Wang <171094685@qq.com>
2015-10-15 10:43:34 +0800

    add a new implement method for the quick sort.

--- sort/quickSort.c
+++ sort/quickSort.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/*
+ *the first method.
+ */
 int getMiddle(int data[],int begin,int end){
 	int front = 0;
 	int tail = end - begin;
@@ -26,21 +29,50 @@ int getMiddle(int data[],int begin,int end){
 	return begin + front;
 }
 
-void _quickSort(int data[],int begin,int end){
+void _quickSort_1(int data[],int begin,int end){
 	if(begin >= end)
 		return ;
 	int middle = getMiddle(data,begin,end);
-	_quickSort(data,begin,middle-1);
-	_quickSort(data,middle+1,end);
+	_quickSort_1(data,begin,middle-1);
+	_quickSort_1(data,middle+1,end);
 }
 
-void quickSort(int data[], int n){
-	if(n < 0 || NULL == data)
+void quickSort_1(int data[], int n){
+	if(n <= 0 || NULL == data)
 		return ;
-	_quickSort(data,0,n-1);	
+	_quickSort_1(data,0,n-1);	
 }
 
+/*
+ *the second method.
+ */
+void _quickSort_2(int nums[],int begin,int end){
+	if(begin >= end)
+		return ;
+	int first = begin;
+	int last = end;
+	int key = nums[begin];
+	while(first < last){
+		while(first<last && nums[last]>key)
+			last --;
+		nums[first] = nums[last];
+		while(first<last && nums[first]<key)
+			first ++;
+		nums[last] = nums[first];
+	}
+	nums[first] = key;
+	_quickSort_2(nums,begin,first-1);
+	_quickSort_2(nums,first+1,end);
+}
+void quickSort(int nums[],int n){
+	if(n<=0 || NULL==nums)
+		return ;
+	_quickSort_2(nums,0,n-1);
+}
 
+/*
+ * test.
+ */
 int main(){
 	int data[10] = {1,9,6,5,8,7,4,3,2,0};
 	quickSort(data,10);
